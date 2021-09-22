65ee96c4d50cf83d392f52034a68c9434abb7cde
Nikolaev Nikita <69678682+NikitkaN@users.noreply.github.com>
2021-09-23 00:18:15 +0300

    New

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var hamburger = document.querySelector(".hamburger");
+var menu = document.querySelector(".menu");
+hamburger.addEventListener("click", function() {
+    hamburger.classList.toggle("is-active");
+    menu.classList.toggle("is-active");
+});
